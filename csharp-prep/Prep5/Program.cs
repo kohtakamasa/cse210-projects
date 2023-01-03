392d9e719ac8851fce371157a8629c37a1468c77
kohtakamasa <kohtakamasa@gmail.com>
2023-01-03 14:50:08 +0000

    prep5 assignment

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -25,6 +25,46 @@ class Program
         necessary.
         */
 
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+
+            return name;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.WriteLine("Enter your favorite number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            return num;
+        }
+
+        static int SquareNumber(int number)
+        {
+            int squared = number * number;
+            return squared;
+        }
+
+        static void DisplayResult(string name, int squared)
+        {
+            Console.WriteLine($"Hi, {name}. The square of your number is {squared}.");
+        }
+
+        DisplayWelcome();
+        string name = PromptUserName();
+        int num = PromptUserNumber();
+
+        int squareNum = SquareNumber(num);
+        DisplayResult(name, squareNum);
+
+
 
 
         /* NOTES
