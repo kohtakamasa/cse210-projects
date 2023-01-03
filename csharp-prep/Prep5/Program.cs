3ba27d8503e8691601d3a3cc97431c4bd3c61499
kohtakamasa <kohtakamasa@gmail.com>
2023-01-03 11:33:36 +0000

    prep5 notes

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -5,5 +5,69 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep5 World!");
+
+        /* 
+        For this assignment, write a C# program that has several simple functions:
+
+        DisplayWelcome - Displays the message, "Welcome to the 
+        Program!"
+        PromptUserName - Asks for and returns the user's name (as a 
+        string)
+        PromptUserNumber - Asks for and returns the user's favorite 
+        number (as an integer)
+        SquareNumber - Accepts an integer as a parameter and returns 
+        that number squared (as an integer)
+        DisplayResult - Accepts the user's name and the squared 
+        number and displays them.
+
+        Your Main function should then call each of these functions 
+        saving the return values and passing data to them as 
+        necessary.
+        */
+
+
+
+        /* NOTES
+        FUNCTIONS
+            - general structure of function 
+                returnType FunctionName(dataType parameter1, dataType parameter2)
+                {
+                    // function_body
+                }
+            - use TitleCase
+            - Need to define return data type (if none --> void)
+
+            void DisplayMessage()
+            {
+                Console.WriteLine("Hello World");
+            }
+
+            void DisplayPersonalMessage(string userName)
+            {
+                Console.WriteLine($"Hello {userName"});
+            }
+
+            int AddNumbers(int first, int second)
+            {
+                int sum = first + second;
+                return sum;
+            }
+
+        FUNCTIONS VS METHODS
+            - member function is a method
+            "If you want to define "regular" standalone function, you 
+            need to use the static keyword. This tells C# that you 
+            want your functions to be able to be called without any 
+            other context."
+            
+            i.e.
+            static void DisplayMessage()
+            {
+                Console.WriteLine("Hello World);
+            }
+        
+        VARIABLE SCOPE 
+            variables declared only in the same body can be used
+        */
     }
 }
